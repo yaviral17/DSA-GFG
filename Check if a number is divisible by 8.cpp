800e71cb33b2a4d745ee560737dcbb9ee24cceb7
Aviral Yadav <yaviral17@gmail.com>
2024-02-28 00:21:00 +0530

    problem of the day done 28-feb-2024

---
+++ Check if a number is divisible by 8.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int DivisibleByEight(string s){
+        int minus = s.length() >= 3 ? 3: s.length();
+        long long num = stoi(s.substr(s.length()-minus,minus));
+        if(num%8 ==0) return 1;
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.DivisibleByEight(S)<<"\n";
+    }
+}
+// } Driver Code Ends
